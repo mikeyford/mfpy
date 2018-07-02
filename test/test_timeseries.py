328f94879078c5a9398c39605e92a5ad14fbb75d
Michael Ford <8105141+mikeyford@users.noreply.github.com>
2018-07-02 23:20:34 +0100

    updated tests

--- test/test_timeseries.py
+++ test/test_timeseries.py
@@ -1,7 +1,8 @@
 import unittest
 import pandas as pd
-from pandas.testing import assert_frame_equal as assert_df
+from pandas.testing import assert_frame_equal
 import numpy as np
+from functools import partial
 from mytools import timeseries
 
 
@@ -25,25 +26,24 @@ df_expected_event_delta = pd.DataFrame({'id': ['1.01', '1.01', '2.02', '3.03' , 
                                          index=[0, 1, 2, 3, 4])
 
 
-def reset_col_order(df):
-    return df.sort_index(axis=1)
+def assert_df(left, right):
+    return assert_frame_equal(left=left.sort_index(axis=1), 
+                              right=right.sort_index(axis=1), check_dtype=False)
 
 
 class TestUtilsFunctions(unittest.TestCase):
   
     def test_sample_id_hists(self):
         df_input_filtered = (df_input.groupby('id')
-                                     .filter(lambda x: sum(x.feature) == 0)) 
-        assert_df(reset_col_order(timeseries.sample_id_hists(df_input_filtered, id_col='id', 
-                                                             additional_sort_col='date', frac=1)),
-                                      reset_col_order(df_expected_false))
+                                     .filter(lambda x: sum(x.feature) == 0))
+        
+        assert_df(timeseries.sample_id_hists(df_input_filtered, id_col='id', additional_sort_col='date', frac=1),
+                  df_expected_false)
         
     
     def test_event_delta(self):
-        assert_df(reset_col_order(timeseries.event_delta(df_input, 'id', 'date', 
-                                                         'feature', period='M').reset_index(drop='id')),
-                                  reset_col_order(df_expected_event_delta),
-                 check_dtype=False)
+        assert_df(timeseries.event_delta(df_input, 'id', 'date', 'feature', period='M').reset_index(drop='id'),
+                  df_expected_event_delta)
 
         
 if __name__ == '__main__':
