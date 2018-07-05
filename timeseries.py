8ba17b6d2b623eb1b534ce481b7a72b5d19f6e32
Michael Ford <mfogeo@gmail.com>
2018-07-05 09:58:00 +0100

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -4,14 +4,17 @@ from functools import partial
 
 def sample_id_hists(df, id_col, additional_sort_col=None, sort=True, **kwargs):
     """
-    Returns subset of pandas dataframe containing complete histories for sample of ids.
+    Generate subsample of complete histories from a dataframe that contains mutiple entries for IDs. 
     
     Args:
         df: pandas dataframe
         id_col: name of column containing ids
         additional_sort_col: name of additional column to use to sort after ids, e.g. 'date'
         sort: boolean, if the results should be sorted by id and additional_sort_col
-        kwargs: arguments of pd.Series.sample() to use for sampling ids e.g. frac=0.1, n=1 
+        kwargs: arguments of pd.Series.sample() to use for sampling ids e.g. frac=0.1, n=1
+        
+    Returns:
+        subset of rows from df
         """
     
     ids = pd.Series(df[id_col].unique())
@@ -47,7 +50,7 @@ def offset_from_first_event(group, date_col, event_col, event, period, find):
 
 def event_delta(df, groupby_col, date_col, event_col, event=True, period='D', find='first'):
     """
-    Returns pandas dataframe with new column containing offset from first event for each group.
+    Calculate offset from first or last event of a specifed type for each IDs history.
     
     Args:
         df: pandas dataframe
@@ -57,6 +60,9 @@ def event_delta(df, groupby_col, date_col, event_col, event=True, period='D', fi
         event: type of cell contents to look for, default is True
         period: string for pd.Series.dt offset alias, e.g. 'M' for months, 'D' for calendar days
         find: 'first' or 'last' to return offset from first or last occurence of event respectively
+        
+    Returns:
+        original pandas dataframe with new column 'event_col_delta' appended
         """
     
     if event not in df[event_col].values:
